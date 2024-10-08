67fa3f0346f4dba7125e12690ec77a4bf4fbf962
unknown <hoanganh170402@gmail.com>
2024-10-08 14:24:13 +0700

    31 - 34 - done

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -43,47 +43,47 @@ class Helper
         return $active == 0 ? '<span class="btn btn-danger btn-sm" >NO</span>' : '<span class="btn btn-success btn-sm" >YES</span>';
     }
 
-//    public static function menus($menus, $parent_id = 0) :string
-//    {
-//        $html = '';
-//        foreach ($menus as $key => $menu) {
-//            if ($menu->parent_id == $parent_id) {
-//                $html .= '
-//                    <li>
-//                        <a href="/danh-muc/' . $menu->id . '-' . Str::slug($menu->name, '-') . '.html">
-//                            ' . $menu->name . '
-//                        </a>';
-//
-//                unset($menus[$key]);
-//
-//                if (self::isChild($menus, $menu->id)) {
-//                    $html .= '<ul class="sub-menu">';
-//                    $html .= self::menus($menus, $menu->id);
-//                    $html .= '</ul>';
-//                }
-//
-//                $html .= '</li>';
-//            }
-//        }
-//
-//        return $html;
-//    }
-//
-//    public static function isChild($menus, $id) : bool
-//    {
-//        foreach ($menus as $menu) {
-//            if ($menu->parent_id == $id) {
-//                return true;
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    public static function price($price = 0, $priceSale = 0)
-//    {
-//        if ($priceSale != 0) return number_format($priceSale);
-//        if ($price != 0)  return number_format($price);
-//        return '<a href="/lien-he.html">Liên Hệ</a>';
-//    }
+    public static function menus($menus, $parent_id = 0) :string
+    {
+        $html = '';
+        foreach ($menus as $key => $menu) {
+            if ($menu->parent_id == $parent_id) {
+                $html .= '
+                    <li>
+                        <a href="/danh-muc/' . $menu->id . '-' . Str::slug($menu->name, '-') . '.html">
+                            ' . $menu->name . '
+                        </a>';
+
+                unset($menus[$key]);
+
+                if (self::isChild($menus, $menu->id)) {
+                    $html .= '<ul class="sub-menu">';
+                    $html .= self::menus($menus, $menu->id);
+                    $html .= '</ul>';
+                }
+
+                $html .= '</li>';
+            }
+        }
+
+        return $html;
+    }
+
+    public static function isChild($menus, $id) : bool
+    {
+        foreach ($menus as $menu) {
+            if ($menu->parent_id == $id) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static function price($price = 0, $priceSale = 0)
+    {
+        if ($priceSale != 0) return number_format($priceSale);
+        if ($price != 0)  return number_format($price);
+        return '<a href="/lien-he.html">Liên Hệ</a>';
+    }
 }
