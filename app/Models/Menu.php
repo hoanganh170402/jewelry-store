749ca6a9a7ede7d0aa5ef8ecdd9367203d1b9e42
unknown <hoanganh170402@gmail.com>
2024-10-17 13:56:34 +0700

    39-42 - done

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -17,8 +17,8 @@ class Menu extends Model
         'active'
     ];
 
-//    public function products()
-//    {
-//        return $this->hasMany(Product::class, 'menu_id', 'id');
-//    }
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'menu_id', 'id');
+    }
 }
